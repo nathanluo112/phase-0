1411361f536f277b4080a517d3440c60b81b7006
Nathan Luo <naipton@gmail.com>
2015-09-19 15:39:55 -0400

    Update files for group research

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -6,19 +6,23 @@ i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
 
 # Person 1's solution
+
 def my_array_finding_method(array, letter)
   array.select do |x|
-    x if x.class == String && x.include?(letter)
-  end
+   x if x.class == String && x.include?(letter)
+ end
 end
 
-def my_hash_finding_method(source, thing_to_find)
+def my_hash_finding_method(hash, num)
   new_array = []
-  hash.each do |key, value|
-    new_array << key if value == num
-  end
+    hash.each do |key, value|
+     new_array << key if value == num
+   end
   new_array
 end
+
+
+
 
 # Identify and describe the Ruby method(s) you implemented.
 #
@@ -49,12 +53,12 @@ end
 
 
 # Person 3
-def my_array_sorting_method(source)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+def my_array_sorting_method(array)
+  return array.sort_by {|x| x.to_s}
 end
 
-def my_hash_sorting_method(source)
-   source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+def my_hash_sorting_method(my_family_pets_ages)
+  my_family_pets_ages.sort_by {|key,val| val}
 end
 
 # Identify and describe the Ruby method(s) you implemented.
@@ -65,26 +69,59 @@ end
 
 # Person 4
 def my_array_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.each do |word|
+    if word.class != Fixnum && word.include?(thing_to_delete)
+      source.delete(word)
+    end
+  end
+
 end
 
+
+########################################
+#   begin
+#   source.delete_if { x.include?(thing_to_delete)}
+#   rescue
+#     next
+#   end
+
+
+# end
+
+
+
+
+############################################
 def my_hash_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
-end
+  source.dup
 
-# Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+  source.delete_if { |key, value| key == thing_to_delete }
+end
 
 
 # Person 5
 def my_array_splitting_method(source)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  new_array = Array.new(2){Array.new}
+  source.each do |element|
+    if element.class == Fixnum
+      new_array[0] << element
+    else
+      new_array[1]<<element
+    end
+  end
+  return new_array
 end
 
 def my_hash_splitting_method(source, age)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  new_array = Array.new(2){Array.new}
+  source.each do |key, value|
+    if value <=age
+      new_array[0] <<[key, value]
+    else
+      new_array[1] <<[key, value]
+    end
+  end
+  return new_array
 end
 
 # Identify and describe the Ruby method(s) you implemented.
