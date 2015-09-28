0b7c8ab94421fb9a05463cbdec47ffd89280b8ae
Nathan Luo <naipton@gmail.com>
2015-09-28 15:51:42 -0400

    Update pair file

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -6,9 +6,9 @@ array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 
 # attempts:
 # ============================================================
-p array[1][1][1][0]
-
 p array[1][1][2][0]
+
+
 # ============================================================
 
 # Hole 2
@@ -18,7 +18,7 @@ hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 
 # attempts:
 # ============================================================
-
+p hash[:outer][:inner]["almost"][3]
 
 
 # ============================================================
@@ -31,7 +31,7 @@ nested_data = {array: ["array", {hash: "finished"}]}
 
 # attempts:
 # ============================================================
-
+p nested_data[:array][1][:hash]
 
 
 # ============================================================
@@ -39,9 +39,35 @@ nested_data = {array: ["array", {hash: "finished"}]}
 # RELEASE 3: ITERATE OVER NESTED STRUCTURES
 
 number_array = [5, [10, 15], [20,25,30], 35]
-
+number_array.map! do |num|
+  if num.kind_of?(Array)
+    num.map!{|num2| num2 + 5}
+  else
+    num + 5
+  end
+end
+p number_array
 
 
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+
+def ly_names(array)
+  array.map! do |word|
+    if word.kind_of?(Array)
+      ly_names(word)
+    else
+      word = word + 'ly'
+    end
+  end
+end
+
+p ly_names(startup_names)
+
+Reflection
+What are some general rules you can apply to nested arrays?
+
+
+What are some ways you can iterate over nested arrays?
+Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
--- week-6/testing.rb
+++ week-6/testing.rb
@@ -1,7 +1,8 @@
-a = {
-  "cookie" => 1,
-  "pie" => 7,
-  "cake"=>5
-}
-puts a.values_at(1)
 
+
+nested_array = [1, ["inner", "array"], 2, 3]
+
+nested_array.each { |element| puts element}
+
+nested_hash = {:outer_key => {:inner_key => "Winner!"}}
+puts nested_hash[:outer_key]
