e7bc4e9b7e483f11f82d7762da9f91ccc742e947
Nathan Luo <naipton@gmail.com>
2015-10-01 13:21:39 -0400

    Add group project files

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,4 @@
+// As a user, I want to have 3 different operations.
+// 1. Calculate the sum of arbitrary amount of numbers
+// 2. Calculate the mean of arbitrary amount of numbers
+// 3. Calculate the median of arbitrary amount of numbers
