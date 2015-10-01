1801967d8584789a557641e8776237cbc729343b
Nathan Luo <naipton@gmail.com>
2015-10-01 11:49:28 -0400

    Add solo challenge file

--- week-7/testing.js
+++ week-7/testing.js
@@ -1,2 +1,33 @@
-a = "12345"
-console.log(a.slice(-3, -1))
+// var a = [1,2,3,4,5];
+// var b = [];
+// b = a.splice(3,1);
+// a.reverse();
+// console.log(b);
+// console.log(a);
+
+var mainChar = new Object();
+mainChar.name = "Main Character";
+mainChar.currentDirection = "up";
+mainChar.speed = 0;
+mainChar.age = 0;
+mainChar.location = [1,1];
+
+
+console.log(mainChar);
+
+var move = function() {
+  this.location = [10,10];
+  console.log("What is this? "+ this.location);
+  this.age += 1;
+}
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+
+mainChar.setAge = setAge;
+mainChar.move = move;
+mainChar.move();
+console.log(mainChar);
+mainChar.setAge(50);
+console.log(mainChar);
+
