5f4d1eb509c4c1c26927daca315d701abfa42ec8
Nathan Luo <naipton@gmail.com>
2015-10-02 12:48:42 -0400

    Update pair challenge file:

---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,34 @@
+ // JavaScript Olympics
+
+// I paired [by myself, with:] on this challenge.
+
+// This challenge took me [#] hours.
+
+
+// Warm Up
+
+
+
+
+// Bulk Up
+
+
+
+
+// Jumble your words
+
+
+
+
+// 2,4,6,8
+
+
+
+// "We built this city"
+
+
+
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+console.log(michaelPhelps.constructor === Athlete)
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+// Reflection
--- week-7/testing.js
+++ week-7/testing.js
@@ -1,33 +1,69 @@
-// var a = [1,2,3,4,5];
-// var b = [];
-// b = a.splice(3,1);
-// a.reverse();
-// console.log(b);
-// console.log(a);
+ // JavaScript Olympics
 
-var mainChar = new Object();
-mainChar.name = "Main Character";
-mainChar.currentDirection = "up";
-mainChar.speed = 0;
-mainChar.age = 0;
-mainChar.location = [1,1];
+// I paired [with: Alison] on this challenge.
+
+// This challenge took me [.5] hours.
 
 
-console.log(mainChar);
+// Bulk Up
+var array = [
+  {name: "Sarah", event: "Single"},
+  {name: "Mike", event: "Double"}
+   ];
 
-var move = function() {
-  this.location = [10,10];
-  console.log("What is this? "+ this.location);
-  this.age += 1;
+var win = function (){
+  console.log(this.name + " win the "+ this.event);
 }
-var setAge = function (newAge) {
-  this.age = newAge;
-};
 
-mainChar.setAge = setAge;
-mainChar.move = move;
-mainChar.move();
-console.log(mainChar);
-mainChar.setAge(50);
-console.log(mainChar);
+for (var i = 0; i < array.length; i++){
+  array[i].win = win;
+  array[i].win();
+}
 
+
+// Jumble your words
+var string1 = "abc";
+
+function reverser(string){
+  var arrayString = string.split('');
+  return arrayString.reverse().join('');
+
+}
+console.log(reverser(string1));
+
+
+// 2,4,6,8
+var nums = [1,2,3,4,5,6,7,8];
+
+function isEven(num){
+  return num%2 == 0;
+}
+
+var even = nums.filter(isEven);
+console.log(even);
+
+
+
+// "We built this city"
+function Athlete(name, age, sport, quote){
+  this.name = name;
+  this.age = age;
+  this.sport = sport;
+  this.quote = quote;
+}
+
+
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+console.log(michaelPhelps.constructor === Athlete)
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+
+// Reflection
+// What JavaScript knowledge did you solidify in this challenge?
+// Function stored in a variable is a very interesting concept. I had some trouble understanding that. After the first release I have a much better understanding of why it use "this" and how "this" is linked to the object.
+
+// What are constructor functions?
+// Constructor functions are constomized function similar to "Object()", which takes more arguments for initializing the object's properties.
+
+// How are constructors different from Ruby classes (in your research)?
+// The constructor functions are not tied to a "class" in JavaScript because there is no "class" in JavaScript, only objects. Though the behavior is very similar to class.
+//If we treat an object with certain kinds of properties as a class, then they can be changed to other "classes" by modifying the object's properties.
