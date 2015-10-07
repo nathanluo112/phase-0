235f44e778a768e33182206c3ee7fb565934e926
Nathan Luo <naipton@gmail.com>
2015-10-07 11:51:13 -0400

    Update pair challenge file

---
+++ week-8/ruby_review.rb
@@ -0,0 +1,63 @@
+# Create a Bingo Scorer (SOLO CHALLENGE)
+
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+
+
+
+
+# sample boards
+
+# horizontal = [[47, 44, 71, 8, 88],
+#               ['x', 'x', 'x', 'x', 'x'],
+#               [83, 85, 97, 89, 57],
+#               [25, 31, 96, 68, 51],
+#               [75, 70, 54, 80, 83]]
+
+# vertical = [[47, 44, 71, 'x', 88],
+#             [22, 69, 75, 'x', 73],
+#             [83, 85, 97, 'x', 57],
+#             [25, 31, 96, 'x', 51],
+#             [75, 70, 54, 'x', 83]]
+
+
+# right_to_left = [['x', 44, 71, 8, 88],
+#                  [22, 'x', 75, 65, 73],
+#                  [83, 85, 'x', 89, 57],
+#                  [25, 31, 96, 'x', 51],
+#                  [75, 70, 54, 80, 'x']]
+
+
+# left_to_right = [[47, 44, 71, 8, 'x'],
+#                   [22, 69, 75, 'x', 73],
+#                   [83, 85, 'x', 89, 57],
+#                   [25, 'x', 96, 68, 51],
+#                   ['x', 70, 54, 80, 83]]
+
+
+
+
+# Initial Solution
+class BingoScorer
+
+  #your code here
+
+end
+
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+# DRIVER TESTS GO BELOW THIS LINE
+# implement tests for each of the methods here:
+
+
+# Reflection
---
+++ week-8/testing.js
@@ -0,0 +1,7 @@
+var a ={
+  'a':1,
+  'b':2,
+  'c':3,
+}
+
+console.log(a.length);
